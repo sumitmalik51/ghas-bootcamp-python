e32702f57ee42601cb3611c3198c97d09f7e5752
Omar Quimbaya <WritingPanda@users.noreply.github.com>
2024-08-08 19:36:59 -0700

    Update routes.py

--- server/routes.py
+++ server/routes.py
@@ -30,7 +30,7 @@ def index():
     return render_template("books.html", books=books)
 
 # Uncomment the following lines of code and make a pull request to see CodeQL in action
-'''
+
 @flaskapp.route("/log_injections")
 def log_injections():
     data = request.args.get("data")
@@ -80,4 +80,3 @@ def get_users():
     except:
         data = str(hostname) + " username not found"
         return data
-'''
